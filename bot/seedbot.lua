10db1f0bd2edaa08d475ffb27a153b2c5971d200
sajjad21321 <cloud9.teamsa@gmail.com>
2016-02-05 00:36:39 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -229,26 +229,11 @@ function create_config( )
     sudo_users = {114779183,111057732,0},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[Admin sudo S.A soft 
+    Im pro to Bot  ")
+    @cLm_A
+    @Xray4
+    
 ]],
     help_text_realm = [[
 Realm Commands:

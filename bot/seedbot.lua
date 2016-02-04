f2e028a7ec5c3b057c75fbb6721d79a486edcae8
sajjad21321 <cloud9.teamsa@gmail.com>
2016-02-04 20:55:02 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {114779183,111057732,0(our_id)},--Sudo users
+    sudo_users = {114779183,111057732,0},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
